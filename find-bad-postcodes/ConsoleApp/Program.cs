244ad359a592b6aaeac1c1c1faf3860ec2ff84f3
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2026-02-23 21:22:41 +0000

    Make 500 requests in parallel

--- find-bad-postcodes/ConsoleApp/Program.cs
+++ find-bad-postcodes/ConsoleApp/Program.cs
@@ -1,40 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
+using System.Collections.Concurrent;
 using ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
 var postcodeEnumerator = new PostcodeEnumerator();
-IEnumerable<string> postcodes = postcodeEnumerator.EnumeratePostcodes("IM14GV")
-    .Take(1000);
+IEnumerable<string> postcodes = postcodeEnumerator.EnumeratePostcodes("IM10AA")
+    .Take(1)
+    ;
 
 string baseUrl = "https://services.gov.im/service/DriversAndVehicles/Vehicle/VehicleLicenceDuplicates/AddressLookup?isAjaxRequest=true";
 
 using var httpClient = new HttpClient();
 
 int successCount = 0;
-List<string> failedPostcodes = new List<string>();
+ConcurrentBag<string> failedPostcodes = new ConcurrentBag<string>();
 string lastTriedPostcode = "-";
+object consoleLock = new object();
 
 Stopwatch stopwatch = Stopwatch.StartNew();
-foreach (string postcode in postcodes)
-{
-    lastTriedPostcode = postcode;
-    string url = $"{baseUrl}&postcode={Uri.EscapeDataString(postcode)}";
-    using HttpResponseMessage response = await httpClient.GetAsync(url);
-    if (!response.IsSuccessStatusCode)
+await Parallel.ForEachAsync(
+    postcodes,
+    new ParallelOptions { MaxDegreeOfParallelism = 500 },
+    async (postcode, cancellationToken) =>
     {
-        failedPostcodes.Add(postcode);
-        Console.WriteLine($"\n\n\n{postcode} - {response.StatusCode} {response.ReasonPhrase}\n{await response.Content.ReadAsStringAsync()}");
-    }
-    else
-    {
-        successCount++;
-    }
-}
+        Volatile.Write(ref lastTriedPostcode, postcode);
+        string url = $"{baseUrl}&postcode={Uri.EscapeDataString(postcode)}";
+        using HttpResponseMessage response = await httpClient.GetAsync(url, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            failedPostcodes.Add(postcode);
+            string body = await response.Content.ReadAsStringAsync(cancellationToken);
+            lock (consoleLock)
+            {
+                Console.WriteLine($"\n\n\n{postcode} - {response.StatusCode} {response.ReasonPhrase}\n{body}");
+            }
+        }
+        else
+        {
+            Interlocked.Increment(ref successCount);
+        }
+    });
 
 Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
 Console.WriteLine($"{successCount} successful postcodes");
 Console.WriteLine($"{failedPostcodes.Count} failed postcodes \n{string.Join(", ", failedPostcodes)}");
-Console.WriteLine($"Last tried postcode: {lastTriedPostcode}");
+Console.WriteLine($"Last tried postcode: {Volatile.Read(ref lastTriedPostcode)}");
